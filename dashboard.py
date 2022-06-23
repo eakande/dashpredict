284001f539ca37b21cc31b0914ff6fcba0b25e0e
eakande <89161472+eakande@users.noreply.github.com>
2022-06-23 12:23:07 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -236,7 +236,7 @@ from dash_bootstrap_components.themes import CYBORG,PULSE,DARKLY
 db=ExplainerDashboard(explainer, [CustomModelTab, CustomModelTab1, CustomPredictionsTab,
                                CustomPredictionsTab2, CustomPredictionsTab3, CustomPredictionsTab4], 
                         title='Macroeconomic Indicator Prediction for Nigeria', header_hide_selector=False,
-                        bootstrap=CYBORG).run()
+                        bootstrap=CYBORG)
 
 
 
